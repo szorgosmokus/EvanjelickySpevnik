d488ae10023ed217c0bf40d3d92f3c2429a2df6b
Zoltan Csanda <zolo.csanda@gmail.com>
2024-01-02 12:51:19 +0100

    BugFix: textové okienko kde sa zadávalo číslo piesne nefungovalo správne - nedalo sa doň písať keď som "vymazal" text po ťuknutí. Namiesto vymazania textu bolo treba zavolať metódu .clear() na controller a už to funguje správne
    Increase version number
    
    Zmemil som popis appky pre android.

--- lib/main.dart
+++ lib/main.dart
@@ -218,7 +218,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         },
                         onTap: () {
                           // I blank out the input in the textfield when the user taps on it
-                          songNumber.text = "";
+                          songNumber.clear();
                         },
                       ),
                     ),
